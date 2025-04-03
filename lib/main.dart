524651f7e045c04a037b5954fb78d424dba56217
frand <frandsen_34@hotmail.dk>
2025-04-03 13:14:45 +0200

    [IMP] Added routes to navigate around

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:deploy_it_app/pages/admin_page.dart';
+import 'package:deploy_it_app/pages/deployment_page.dart';
 import 'package:flutter/material.dart';
 import 'pages/login_page.dart';
+import 'pages/status_page.dart';
+import 'pages/sign_up_page.dart';
+import 'pages/profile_page.dart';
+import 'pages/pay_status.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +14,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-           home: LoginPage(),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => SignUpPage(),
+        '/status': (context) => StatusPage(),//get token and put it in or have it as an global value
+        '/profile': (context) => ProfilePage(),
+        '/paid': (context) => PayStatus(),
+        '/deploy': (context) => DeploymentPage(),
+        '/admin': (context) => AdminPage(),
+      },
     );
   }
 }
