e2aa46cdc9663db30b2b4b0b650e625e82b0557c
frand <frandsen_34@hotmail.dk>
2025-04-16 16:02:50 +0200

    [IMP] Add dynamic theme support using ValueListenableBuilder and ThemeController

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:deploy_it_app/pages/admin_page.dart';
 import 'package:deploy_it_app/pages/deployment_page.dart';
-import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
-import 'pages/status_page.dart';
-import 'pages/sign_up_page.dart';
-import 'pages/profile_page.dart';
-import 'pages/pay_status.dart';
+import 'package:deploy_it_app/pages/login_page.dart';
+import 'package:deploy_it_app/pages/status_page.dart';
+import 'package:deploy_it_app/pages/sign_up_page.dart';
+import 'package:deploy_it_app/pages/profile_page.dart';
+import 'package:deploy_it_app/pages/pay_status.dart';
+import 'package:deploy_it_app/components/theme_controller.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -16,17 +18,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => LoginPage(),
-        '/signup': (context) => SignUpPage(),
-        '/status': (context) => StatusPage(),//get token and put it in or have it as an global value
-        '/profile': (context) => ProfilePage(),
-        '/paid': (context) => PayStatus(),
-        '/deploy': (context) => DeploymentPage(),
-        '/admin': (context) => AdminPage(),
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: themeNotifier,
+      builder: (_, ThemeMode currentMode, __) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Deploy-It',
+          theme: ThemeData.light(),
+          darkTheme: ThemeData.dark(),
+          themeMode: currentMode,
+          initialRoute: '/login',
+          routes: {
+            '/login': (context) => LoginPage(),
+            '/signup': (context) => SignUpPage(),
+            '/status': (context) => StatusPage(),
+            '/profile': (context) => ProfilePage(),
+            '/paid': (context) => PayStatus(),
+            '/deploy': (context) => DeploymentPage(),
+            '/admin': (context) => AdminPage(),
+          },
+        );
       },
     );
   }
