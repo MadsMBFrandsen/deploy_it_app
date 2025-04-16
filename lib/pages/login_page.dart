d5c2ab854445a6e9b223ffd3eb4ac07be0f19647
frand <frandsen_34@hotmail.dk>
2025-04-16 16:01:54 +0200

    [IMP] Refactor LoginPage for API-based login, user profile fetching, and SharedPreferences storage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:deploy_it_app/components/my_button.dart';
 import 'package:deploy_it_app/components/my_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:deploy_it_app/components/api_calls_temp.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
@@ -8,9 +10,48 @@ class LoginPage extends StatelessWidget {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
-  void signUserIn(BuildContext context){
-    Navigator.pushReplacementNamed(context, '/status');
+  void signUserIn(BuildContext context) async {
+    final username = usernameController.text.trim();
+    final password = passwordController.text;
+
+    try {
+      //SnackBar(content: Text('username ${username} password ${password}'));
+      final loginResponse = await ApiService.login(username, password);
+
+      //final token = loginResponse['token'];
+      final role = loginResponse['role'];
+
+      String email = '';
+      try {
+        final profileResponse = await ApiService.getUserProfile();
+        email = profileResponse['email'];
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Get User Info failed: ${e.toString()}')),
+        );
+      }
+
+      // Save to SharedPreferences
+      final prefs = await SharedPreferences.getInstance();
+     // await prefs.setString('auth_token', token);
+      await prefs.setString('user_role', role);
+      await prefs.setString('user_name', username);
+      await prefs.setString('user_email', email);
+
+     // print("Token saved: $token");
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Welcome $username You Are A ($role)')),
+      );
+
+      Navigator.pushReplacementNamed(context, '/status');
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Login failed: ${e.toString()}')),
+      );
+    }
   }
+
 
   @override
   Widget build(BuildContext context) {
