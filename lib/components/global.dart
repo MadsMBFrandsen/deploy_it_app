ab21192cb8adb8be66f1586882c5014318287de0
frand <frandsen_34@hotmail.dk>
2025-04-16 16:06:02 +0200

    [IMP] Readies to have a global import place

--- lib/components/global.dart
+++ lib/components/global.dart
@@ -1,2 +1,2 @@
-import 'package:flutter/material.dart';
-import 'package:deploy_it_app/components/navigation_bar.dart';
+// import 'package:flutter/material.dart';
+// import 'package:deploy_it_app/components/navigation_bar.dart';
