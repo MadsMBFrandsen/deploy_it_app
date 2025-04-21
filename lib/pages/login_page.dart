44c7d4b1bcae1b3997932ad2cef9cabd2590a157
mads frandsen <frandsen_34@hotmail.dk>
2025-04-21 21:46:11 +0200

    [IMP] Update LoginPage to new API with email login, token storage, and role handling

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,47 +1,41 @@
 import 'package:deploy_it_app/components/my_button.dart';
 import 'package:deploy_it_app/components/my_textfield.dart';
 import 'package:flutter/material.dart';
-import 'package:deploy_it_app/components/api_calls_temp.dart';
+import 'package:deploy_it_app/components/api_calls.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
-  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
   void signUserIn(BuildContext context) async {
-    final username = usernameController.text.trim();
-    final password = passwordController.text;
+    //final email = emailController.text.trim();
+    //final password = passwordController.text;
+
+    final email = "admin@example.org";
+    final password = "KrBFrF6HBP8z8VBDmd9y";
+
 
     try {
-      //SnackBar(content: Text('username ${username} password ${password}'));
-      final loginResponse = await ApiService.login(username, password);
+      final loginResponse = await ApiService.login(email, password);
 
-      //final token = loginResponse['token'];
-      final role = loginResponse['role'];
-
-      String email = '';
-      try {
-        final profileResponse = await ApiService.getUserProfile();
-        email = profileResponse['email'];
-      } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Get User Info failed: ${e.toString()}')),
-        );
-      }
+      final token = loginResponse['token'];
+      final user = loginResponse['user'];
+      final role = user['role'];
+      final name = user['name'];
+      final userEmail = user['email'];
 
       // Save to SharedPreferences
       final prefs = await SharedPreferences.getInstance();
-     // await prefs.setString('auth_token', token);
+      await prefs.setString('auth_token', token);
       await prefs.setString('user_role', role);
-      await prefs.setString('user_name', username);
-      await prefs.setString('user_email', email);
-
-     // print("Token saved: $token");
+      await prefs.setString('user_name', name);
+      await prefs.setString('user_email', userEmail);
 
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Welcome $username You Are A ($role)')),
+        SnackBar(content: Text('Welcome $name! You are a ($role)')),
       );
 
       Navigator.pushReplacementNamed(context, '/status');
@@ -51,6 +45,7 @@ class LoginPage extends StatelessWidget {
       );
     }
   }
+
 
 
   @override
@@ -88,8 +83,8 @@ class LoginPage extends StatelessWidget {
             SizedBox(height: 25),
 
             MyTextField(
-              controller: usernameController,
-              hintText: 'Username',
+              controller: emailController,
+              hintText: 'Email',
               obscureText: false,
             ),
 
@@ -98,7 +93,7 @@ class LoginPage extends StatelessWidget {
             MyTextField(
               controller: passwordController,
               hintText: 'Password',
-              obscureText: true,
+              obscureText: false,
             ),
 
 
